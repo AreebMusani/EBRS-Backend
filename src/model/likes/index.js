cffb79235374db8de5751bdd37464f3a3c21263d
AreebMusani <areeb1805a@aptechgdn.net>
2024-08-11 20:09:14 +0500

    chore: added timestamps in likes table

--- src/model/likes/index.js
+++ src/model/likes/index.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const like = new mongoose.Schema({
-  song: { type: mongoose.Schema.Types.ObjectId, ref: "Songs" },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-});
+  song: { type: mongoose.Schema.Types.ObjectId, ref: "Songs", required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, },
+}, {timestamps: true});
 
 const Like = mongoose.model("like", like);
 
