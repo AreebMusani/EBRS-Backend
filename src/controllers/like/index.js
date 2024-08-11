68a7b17e48e8c3f1965fc1f97da6d9e3bc09fef6
AreebMusani <areeb1805a@aptechgdn.net>
2024-08-11 20:08:53 +0500

    fix: get userId spelling and response message

--- src/controllers/like/index.js
+++ src/controllers/like/index.js
@@ -3,24 +3,22 @@ const Songs = require("../../model/songs");
 const User = require("../../model/user");
 
 exports.like = async (req, res) => {
-  const { songId, usedId } = req.body;
-
+  const { songId, userId } = req.body;
+  console.log(req.body);
   try {
-    const exist = await like.findOne({ user: usedId, song: songId });
+    const exist = await like.findOne({song: songId, user: userId});
     if (exist) {
       throw new Error("Already liked");
     }
-
     const likeSong = await like.create({
       song: songId,
-      user: usedId,
+      user: userId,
     });
 
     if (!likeSong) {
       throw new Error("Something went wrong");
     }
-
-    res.status(201).send({ message: "Song liked successfully" });
+    res.status(201).send({ message: "Added into Favourites..." });
   } catch (e) {
     res.status(400).send({ message: e?.message });
   }
@@ -39,30 +37,32 @@ exports.getLikedSongs = async (req, res) => {
         return res.status(200).send({ data: [] });
     }
 
-    res.status(200).send({ data: likedSongs});
+    res.status(200).send({ message: "Fetched recommendation list...", data: likedSongs});
   } catch (e) {
     res.status(400).send({ message: e?.message });
   }
 };
 
 exports.unlike = async (req, res) => {
-  const { songId, usedId } = req.body;
+  const { songId, userId } = req.body;
+  console.log(req.body);
+  
 
   try {
-    const exist = await like.findOne({ user: usedId, song: songId });
+    const exist = await like.findOne({ user: userId, song: songId });
 
     if (!exist) {
       throw new Error("This song is not like");
     }
 
     const unlikeSong = await like
-      .findOneAndDelete({ user: usedId, song: songId })
+      .findOneAndDelete({ user: userId, song: songId })
 
     if (!unlikeSong) {
       throw new Error("Something went wrong");
     }
 
-    res.status(201).send({ message: "Song unliked successfully" });
+    res.status(201).send({ message: "Removed from favourites..." });
   } catch (e) {
     res.status(400).send({ message: e?.message });
   }
